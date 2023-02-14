2194c419fca7a5176e3837cc14e26ae9809de62c
OsmanVergil <41418985+OsmanVergil@users.noreply.github.com>
2023-02-14 23:54:56 +0300

    Add files via upload

--- 1/Circle/circle.cpp
+++ 1/Circle/circle.cpp
@@ -1,6 +1,11 @@
 #include "circle.h"
 #include <cmath>
 
+Circle :: Circle() {
+    radius = 0;
+    x_center = 0;
+    y_center = 0;
+};
 Circle :: Circle(float r, float x, float y) {
     radius = r;
     x_center = x;
@@ -12,9 +17,28 @@ void Circle :: set_circle(float r, float x, float y) {
     y_center = y;
 }
 float Circle :: square() {
-    return 3.14 * radius * radius;
+    return M_PI * radius * radius;
+}
+bool Circle :: triangle_around(float a, float b, float c) {
+    float cos = (a * a + b * b - c * c) / (2 * a * b);
+    float sin = sqrt(1 - cos * cos);
+    return (c / sin) == (2 * radius);
+}
+bool Circle :: triangle_in(float a, float b, float c) {
+    double p = (a + b + c) / 2;
+    double s = sqrt(p * (p - a) * (p - b) * (p - c));
+    return radius == (s / p);
 }
 bool Circle :: check_circle(float r, float x_cntr, float y_cntr) {
     float distBetweenCenters = sqrt(pow(x_center - x_cntr, 2) + pow(y_center - y_cntr, 2));
-    return true;
+    bool isInside = false;
+    if (r > radius) {
+        if (pow(x_center - x_cntr, 2) + pow(y_center - y_cntr, 2) <= (r * r)) isInside = true;
+        if (isInside) return (distBetweenCenters + radius) > r;
+        else return distBetweenCenters < (r + radius);
+    } else {
+        if (pow(x_center - x_cntr, 2) + pow(y_center - y_cntr, 2) <= (radius * radius)) isInside = true;
+        if (isInside) return (distBetweenCenters + r) > radius;
+        else return distBetweenCenters < (r + radius);
+    }
 }
--- 1/Circle/circle.h
+++ 1/Circle/circle.h
@@ -2,6 +2,7 @@ class Circle {
     private:
     float radius, x_center, y_center;
     public:
+    Circle();
     Circle(float r, float x, float y);
     void set_circle(float r, float x, float y);
     float square();
